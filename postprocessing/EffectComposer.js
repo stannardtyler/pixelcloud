101c34d64f444499cba7b49a5c5e13ff9aa82011
stannardtyler <tstannard64@gmail.com>
2024-12-04 22:51:44 -0800

    fixed file path

--- postprocessing/EffectComposer.js
+++ postprocessing/EffectComposer.js
@@ -10,7 +10,7 @@ import {
   WebGLRenderTarget,
 } from "/build/three.module.js";
 import { CopyShader } from "../shaders/CopyShader.js";
-import { ShaderPass } from "./postprocessing/ShaderPass.js";
+import { ShaderPass } from "./ShaderPass.js";
 import { MaskPass } from "/postprocessing/MaskPass.js";
 import { ClearMaskPass } from "/postprocessing/MaskPass.js";
 
